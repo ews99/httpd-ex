73d30120bdb7959c2ee4e830b10ff59dddcc2d61
ews99 <github@reflexlight.nl>
2018-07-21 23:21:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <h1>Pixelflut nodes overview</h1>
 <?php 
 echo "Version 1.0";
-echo $_ENV['MARIADB-HOSTNAME'];
-echo $_ENV['MARIADB-DATABASE'];
-echo $_ENV['MARIADB-USERNAME'];
+echo $_ENV['MARIADB_HOSTNAME'];
+echo $_ENV['MARIADB_DATABASE'];
+echo $_ENV['MARIADB_USERNAME'];
 ?>
