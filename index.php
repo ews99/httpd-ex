36b056ab0d3c4f80c7ab12183dc995ba9894c89e
ews99 <github@reflexlight.nl>
2018-07-22 09:36:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <html>
 <h1>Pixelflut nodes overview</h1>
-<?php 
-echo "Version 1.0";
-echo $_ENV['MARIADB_HOSTNAME'];
-echo $_ENV['MARIADB_DATABASE'];
-echo $_ENV['MARIADB_USERNAME'];
+Version 1.1
+
+
+<table border="1">
+    <tr>
+        <th>Hostname</th>
+        <th>IP Address</th>
+        <th>Deployment state</th>
+        <th>Last seen</th>
+    </tr>
+<?php
+
+    $mysql_query="SELECT * FROM pixelflut_nodes";
+    $mysqli = new mysqli($_ENV['MARIADB_HOSTNAME'], $_ENV['MARIADB_USERNAME'], $_ENV['MARIADB_PASSWORD'], $_ENV['MARIADB_DATABASE']);
+    if ($mysqli->connect_errno) {
+        die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+    }
+    $res=$mysqli->query($mysql_query);
+    while ($row = $res->fetch_assoc()) {
+        echo "    <tr>\n";
+        echo "        <td>".$row['hostname']."</td>\n";
+        echo "        <td>".$row['ipaddress']."</td>\n";
+        echo "        <td>".$row['deployment_state']."</td>\n";
+        echo "        <td>".$row['last_checkin']."</td>\n";
+        echo "    </tr>\n";
+    }
+
 ?>
+</table>
