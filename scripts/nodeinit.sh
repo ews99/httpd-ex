060b717c69c5fba5e34dc336861babef582136a9
ews99 <github@reflexlight.nl>
2018-07-22 13:31:10 +0200

    Create nodeinit.sh

---
+++ scripts/nodeinit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/ews99/pixelflut-nodes/master/scripts/nodeupdate.sh -q -O /root/nodeupdate.sh
+chmod 0755 /root/nodeupdate.sh
+
+echo "*/1 * * * * root /root/nodeupdate.sh >/dev/null 2>&1" > /etc/cron.d/nodeupdate
+echo "02 * * * * root /root/nodeinit.sh >/dev/null 2>&1" >> /etc/cron.d/nodeupdate
