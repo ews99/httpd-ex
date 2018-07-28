69115aa755368f6176ebada8edd93e4b28c26bb1
ews99 <github@reflexlight.nl>
2018-07-28 23:21:00 +0200

    Update nodeupdate.sh

--- scripts/nodeupdate.sh
+++ scripts/nodeupdate.sh
@@ -4,7 +4,7 @@ HOSTNAME=`hostname`
 HOSTIP=`hostname -I`
 IPADDR=`gethostip -d ${HOSTIP} | head -n 1`
 IPADDRHEX=`gethostip -x ${HOSTIP} | head -n 1`
-DOMAINNAME="infra.local"
+DOMAINNAME="veetikut.com"
 URL="http://pixelflut.apps.veetikut.com/"
 
 if [ "${HOSTNAME:0:4}" != "node" ] # first 4 characters should be "node"
