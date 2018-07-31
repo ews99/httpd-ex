36bde28b40d191cd0dc076d679c14403b6f2135d
ews99 <github@reflexlight.nl>
2018-07-31 18:45:56 +0200

    Update script.php

--- script.php
+++ script.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($_GET['action']) && !empty($_GET['action'])) { $action=htmlspecialchars($_GET['action']); }
 if (isset($_GET['state']) && !empty($_GET['state'])) { $state=htmlspecialchars($_GET['state']); }
+if (isset($_GET['tag']) && !empty($_GET['tag'])) { tag=htmlspecialchars($_GET['tag']); }
 if (isset($_GET['hostname']) && !empty($_GET['hostname'])) { $hostname=htmlspecialchars($_GET['hostname']); }
 if (isset($_GET['ipaddress']) && !empty($_GET['ipaddress'])) { $ipaddress=htmlspecialchars($_GET['ipaddress']); }
 
@@ -33,6 +34,19 @@ case 'update':
         }
         $mysqli->query($mysql_query);
         echo "Updated hostname: ".$hostname." to state: ".$state."\n";
+    }
+    if (!empty($tag) && !empty($hostname)) {
+        $mysql_query="";
+        $mysql_query.="UPDATE pixelflut_nodes";
+        $mysql_query.=" SET `tag`='".$tag."'";
+        $mysql_query.=" WHERE `hostname`='".$hostname."'";
+
+        $mysqli = new mysqli($_ENV['MARIADB_HOSTNAME'], $_ENV['MARIADB_USERNAME'], $_ENV['MARIADB_PASSWORD'], $_ENV['MARIADB_DATABASE']);
+        if ($mysqli->connect_errno) {
+            die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+        }
+        $mysqli->query($mysql_query);
+        echo "Updated tag for hostname: ".$hostname." to tag: ".$tag."\n";
     }
     break;
 
