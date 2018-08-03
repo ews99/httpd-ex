37cc0b2ba2632ed49808a1727d6195ace9c20cf6
ews99 <github@reflexlight.nl>
2018-08-03 13:49:30 +0200

    Update nodeupdate.sh

--- scripts/nodeupdate.sh
+++ scripts/nodeupdate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 HOSTNAME=`hostname`
-HOSTIP=`hostname -I`
+HOSTIP=`hostname -i`
 IPADDR=`gethostip -d ${HOSTIP} | head -n 1`
 IPADDRHEX=`gethostip -x ${HOSTIP} | head -n 1`
+IFACE=`ip route show default | awk '/default/ {print $5}'`
+MACADDR=`cat /sys/class/net/${IFACE}/address`
 DOMAINNAME="veetikut.com"
 URL="http://pixelflut.apps.veetikut.com/"
 
@@ -14,4 +16,4 @@ then
     HOSTNAME=`hostname`
 fi
 
-curl "${URL}/script.php?action=heartbeat&hostname=${HOSTNAME}&ipaddress=${IPADDR}"
+curl "${URL}/script.php?action=heartbeat&hostname=${HOSTNAME}&ipaddress=${IPADDR}&macaddress=${MACADDR}&iface=${IFACE}"
